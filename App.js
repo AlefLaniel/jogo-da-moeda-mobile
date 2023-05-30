0a071f6c16041189dcc5b1222d803f989aac1e6e
AlefLaniel <alef6940@gmail.com>
2023-05-30 12:56:12 -0300

    atualização do app

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Container, Title, Image, Result, Button, TitleButton } from "./style";
+import { Audio } from "expo-av";
+import ElevatedView from "react-native-elevated-view";
+import { useFonts, Inter_900Black, Inter_400Regular, Inter_600SemiBold } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+  const [text, setText] = React.useState("Jogue a Moeda");
+  const [image, setImage] = React.useState(
+    `${require("./assets/images/cara.png")}`
+  );
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/somdamoeda.mp3")
+    );
+    setSound(sound);
+
+    setImage(`${require("./assets/images/girando.gif")}`);
+    console.log("Playing Sound");
+    setTimeout(resultadoFinal, 2000);
+    await sound.playAsync();
+  }
+
+  function resultadoFinal() {
+    let random = Math.floor(Math.random() * 2);
+    if (random === 0) {
+      setText("Deu Cara \n Gire novamente se quiser ☺");
+      setImage(`${require("./assets/images/cara.png")}`);
+    } else {
+      setText("Deu Coroa \n Gire novamente se quiser ☺");
+      setImage(`${require("./assets/images/coroa.png")}`);
+    }
+  }
+
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_400Regular,
+    Inter_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Ola Alef Laniel</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Title>Jogo da Moeda</Title>
+      <Image id="moeda" source={image} />
+      <Result style={{ fontFamily: 'Inter_600SemiBold' }}>{text}</Result>
+      <ElevatedView elevation={3} style={styles.stayElevated}>
+        <Button onPress={playSound}>
+          <TitleButton>Jogar Dinheiro</TitleButton>
+        </Button>
+      </ElevatedView>
+
+      <Text style={{ bottom: 0 }}>Desenvolvido por Alef Laniel ☺</Text>
+      <StatusBar style="dark" translucent />
+    </Container>
   );
 }
 
-
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#e2e1e0",
+  },
+
+  stayElevated: {
+    width: 150,
+    height: 50,
+    margin: 10,
+    backgroundColor: "#7FFFD4",
     alignItems: 'center',
-    justifyContent: 'center',
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20
   },
 });
---
+++ style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  padding: 40px;
+  padding-top: 60px;
+  width: 100%;
+  height: 100%;
+`;
+
+
+export const Title = styled.Text`
+    color: black;
+    font-weight: bold;
+    font-size: 20px;
+    margin-bottom: 20px;
+    color: #4D4A44;
+`;
+
+export const Image = styled.Image`
+    margin-top: 20px;
+`;
+
+export const Result = styled.Text`
+     color: black;
+     margin: 30px;
+     font-weight: 300;
+     text-align:center;
+     font-size: 20px;
+`;
+
+export const Button = styled.TouchableOpacity`
+
+`;
+
+export const TitleButton = styled.Text`
+    font-weight: bold;
+    color: #4D4A44;
+`;
